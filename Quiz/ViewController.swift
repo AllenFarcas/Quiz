2ff7f9fb12af12dc7c95c998db21b00ca6beeca4
Allen Farcas <allen.farcas@student.upt.ro>
2018-12-07 11:42:49 +0200

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Allen-Jasmin Farcas on 07/12/2018.
+//  Copyright © 2018 Allen-Jasmin Farcas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
